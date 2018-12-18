048072729c5b421e064b9ac81605beb0505402c3
Red Hat VDO Team <vdo-devel@redhat.com>
2018-12-18 04:23:40 -0500

    137596 by sweettea on 2018/12/18 (pair: sclafani, joe) [VDOUP-3] Use only submitMetadata in base code.
    .../physicalLayer.h
    Added a declaration of submitMetadataVIO(), to replace former calls
    to layer->{read,write}Metadata().
    
    .../vio.c
    Updated launchMetadataVIO() to call submitMetadataVIO().
    
    .../physicalLayer.c
    Added implementations of submitMetadataVIO().
    
    Co-authored-by: sweettea <sweettea@redhat.com>
    Co-authored-by: sclafani <sclafani@redhat.com>
    Co-authored-by: joe <jochapma@redhat.com>

--- utils/vdo/user/physicalLayer.c
+++ utils/vdo/user/physicalLayer.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/user/physicalLayer.c#5 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/user/physicalLayer.c#6 $
  */
 
 #include "physicalLayer.h"
@@ -48,6 +48,12 @@ ThreadID getCallbackThreadID(void)
 
 /**********************************************************************/
 void destroyVIO(VIO **vioPtr __attribute__((unused)))
+{
+  ASSERT_LOG_ONLY(false, "Stubs are never called");
+}
+
+/**********************************************************************/
+void submitMetadataVIO(VIO *vio __attribute__((unused)))
 {
   ASSERT_LOG_ONLY(false, "Stubs are never called");
 }
