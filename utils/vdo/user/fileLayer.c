b0d2db42a24c9eb1e7a77ab6872e4cb5737fd025
Red Hat VDO Team <vdo-devel@redhat.com>
2020-03-26 00:04:37 -0400

    143066 by jshimkus on 2020/03/26 (pair: joe) Detypedef physicalLayer.h (VDOUP-6)
    .../physicalLayer.h
    Detypedef to kernel standards.
    
    .../fileLayer.c
    Adapt to the above changes.
    
    Co-authored-by: jshimkus <jshimkus@redhat.com>
    Co-authored-by: joe <jochapma@redhat.com>

--- utils/vdo/user/fileLayer.c
+++ utils/vdo/user/fileLayer.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/user/fileLayer.c#4 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/user/fileLayer.c#5 $
  */
 
 #include "fileLayer.h"
@@ -181,7 +181,7 @@ static void vacuousFlush(struct vdo_flush **vdoFlush __attribute__((unused)))
 /**
  * Free a FileLayer and NULL out the reference to it.
  *
- * Implements LayerDestructor.
+ * Implements layer_destructor.
  *
  * @param layerPtr  A pointer to the layer to free
  **/
