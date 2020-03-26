eda54d863a24aeda30058b01c7df467f7804589b
Red Hat VDO Team <vdo-devel@redhat.com>
2020-03-26 00:01:18 -0400

    143062 by bjohnston on 2020/03/26 (pair: sclafani) [VDOUP-6] Decamelcase VDOStatistics struct stat.
    .../statistics.h
    .../KernelStatistics.py
    .../VDOStatistics.py
    Rename asStruct field and move it alphabetically.
    
    .../types.h
    Rename type used for compiling.
    
    .../packer.c
    Update comment referencing getVDOStatistics
    
    .../vdo.h
    .../vdo.c
    Update base code to use new name.
    
    Co-authored-by: bjohnston <bjohnsto@redhat.com>
    Co-authored-by: sclafani <sclafani@redhat.com>

--- vdo-manager/statistics/VDOStatistics.py
+++ vdo-manager/statistics/VDOStatistics.py
@@ -200,11 +200,11 @@ class VDOStatistics(StatStruct):
       Uint64Field("physicalBlocks"),
       # number of logical blocks
       Uint64Field("logicalBlocks"),
-      Uint64Field("oneKBlocks", label = "1K-blocks", derived = "$physicalBlocks * $blockSize // 1024"),
-      Uint64Field("oneKBlocksUsed", label = "1K-blocks used", derived = "($dataBlocksUsed + $overheadBlocksUsed) * $blockSize // 1024", available = "not $inRecoveryMode"),
-      Uint64Field("oneKBlocksAvailable", label = "1K-blocks available", derived = "($physicalBlocks - $dataBlocksUsed - $overheadBlocksUsed) * $blockSize // 1024", available = "not $inRecoveryMode"),
+      Uint64Field("oneKBlocks", derived = "$physicalBlocks * $blockSize // 1024", label = "1K-blocks"),
+      Uint64Field("oneKBlocksUsed", derived = "($dataBlocksUsed + $overheadBlocksUsed) * $blockSize // 1024", label = "1K-blocks used", available = "not $inRecoveryMode"),
+      Uint64Field("oneKBlocksAvailable", available = "not $inRecoveryMode", derived = "($physicalBlocks - $dataBlocksUsed - $overheadBlocksUsed) * $blockSize // 1024", label = "1K-blocks available"),
       Uint8Field("usedPercent", derived = "int((100 * ($dataBlocksUsed + $overheadBlocksUsed) // $physicalBlocks) + 0.5)", available = "((not $inRecoveryMode) and ($mode != \"read-only\"))"),
-      Uint8Field("savings", derived = "int(100 * ($logicalBlocksUsed - $dataBlocksUsed) // $logicalBlocksUsed) if ($logicalBlocksUsed > 0) else -1", display = False, available = "not $inRecoveryMode"),
+      Uint8Field("savings", available = "not $inRecoveryMode", display = False, derived = "int(100 * ($logicalBlocksUsed - $dataBlocksUsed) // $logicalBlocksUsed) if ($logicalBlocksUsed > 0) else -1"),
       Uint8Field("savingPercent", available = "((not $inRecoveryMode) and ($mode != \"read-only\"))", derived = "$savings if ($savings >= 0) else NotAvailable()"),
       # Size of the block map page cache, in bytes
       Uint64Field("blockMapCacheSize"),
