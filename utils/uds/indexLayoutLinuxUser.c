435663b94ec0385804fcf5f5f27e855ab757aba9
Red Hat VDO Team <vdo-devel@redhat.com>
2020-04-06 02:05:24 -0400

    143076 by jwiele on 2020/03/27 (pair: sweettea) Merge 142974, 142975, 142977 from linux-uds to Krusty. 142974 by jwiele on 2020/03/20 (pair: sclafani) UDSUP-6 VDOUP-10 Use built-in byte ordered, unaligned access functions.
    This is step one. Subsequent steps will update UDS and VDO to
    use the kernel style functions and then remove the wrapper
    functions defined in numeric.h such as getInt64LE,
    storeInt64LE, etc. Then a layer of buffering can be removed
    where appropriate.
    
    .../numeric.h
    Use the kernel's built-in unaligned byte order aware get/put
    functions instead of our own carefully crafted functions.
    Use the kernel's uint8_t type instead of byte.
    
    .../userLinux/uds/numericDefs.h
    Define functions for user space matching the kernel built-in
    functions using compiler built-in functions similar to what
    fio does. See fio/os.h.
    
    .../numeric.c
    Remove static assertions about custom functions that are no
    longer needed.
    
    142975 by jwiele on 2020/03/20 (pair: sclafani)
    UDSUP-1 remove unused stuff from numeric.
    
    .../numeric.h
    Remove multiplyWouldOverflow as nothing calls it anymore.
    
    .../numeric.c
    Remove numeric.c entirely as the only things left in it are
    STATIC_ASSERT_ALIGNOF and multiplyWouldOverflow, neither of
    which is used anymore.
    
    142977 by jwiele on 2020/03/20 (pair: msakai, sclafani)
    UDSUP-1 Build fix: don't try to build compile file numeric.c.
    
    .../Makefile
    Reference to object file numeric.o removed from the Makefile
    because source file numeric.c was deleted by change 142975.
    
    143184 by jshimkus on 2020/04/03 (pair: sweettea)
    Merge 143140 to krusty.
    143140 by jshimkus on 2020/04/02 (pair: sweettea)
    Reindent indexLayout*.[ch] (USDUP-1)
    
    This is an otherwise unedited output of the clang-format tool;
    done as a separate change to simplify later merges.
    
    .../indexLayout.h
    .../indexLayout.c
    .../indexLayoutParser.h
    .../indexLayoutParser.c
    .../indexLayoutLinuxUser.c
    Reindent according to kernel standards.
    
    143217 by jwiele on 2020/04/06 (pair: sclafani, jshimkus)
    Adjust for things removed and added by Krusty change 143076.
    
    Change 143076 is the merge of 142974, 142975, and 142977 from
    linux-uds to Krusty.
    
    The GET_UNALIGED macro was removed and the file numericDefs.h
    was resurrected by change 142974:
    UDSUP-6 VDOUP-10 Use built-in byte ordered, unaligned access
    functions.
    
    The entire file numeric.c was removed by change 142975:
    UDSUP-1 remove unused stuff from numeric.
    
    .../Makefile
    Don't try to build numeric.o from deleted file numeric.c.
    
    Co-authored-by: jwiele <jwiele@redhat.com>
    Co-authored-by: sweettea <sweettea@redhat.com>
    Co-authored-by: sclafani <sclafani@redhat.com>
    Co-authored-by: msakai <msakai@redhat.com>
    Co-authored-by: jshimkus <jshimkus@redhat.com>

--- utils/uds/indexLayoutLinuxUser.c
+++ utils/uds/indexLayoutLinuxUser.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/uds-releases/krusty/userLinux/uds/indexLayoutLinuxUser.c#1 $
+ * $Id: //eng/uds-releases/krusty/userLinux/uds/indexLayoutLinuxUser.c#2 $
  */
 
 #include "errors.h"
@@ -28,55 +28,57 @@
 #include "uds.h"
 
 /*****************************************************************************/
-int makeIndexLayout(const char              *name,
-                    bool                     newLayout,
-                    const UdsConfiguration   config,
-                    IndexLayout            **layoutPtr)
+int makeIndexLayout(const char *name,
+		    bool newLayout,
+		    const UdsConfiguration config,
+		    IndexLayout **layoutPtr)
 {
-  char     *file   = NULL;
-  uint64_t  offset = 0;
-  uint64_t  size   = 0;
+	char *file = NULL;
+	uint64_t offset = 0;
+	uint64_t size = 0;
 
-  LayoutParameter parameterTable[] = {
-    { "file",   LP_STRING | LP_DEFAULT, { .str = &file   }, false },
-    { "size",   LP_UINT64,              { .num = &size   }, false },
-    { "offset", LP_UINT64,              { .num = &offset }, false },
-  };
+	LayoutParameter parameterTable[] = {
+		{ "file", LP_STRING | LP_DEFAULT, { .str = &file }, false },
+		{ "size", LP_UINT64, { .num = &size }, false },
+		{ "offset", LP_UINT64, { .num = &offset }, false },
+	};
 
-  char *params = NULL;
-  int result = duplicateString(name, "makeIndexLayout parameters", &params);
-  if (result != UDS_SUCCESS) {
-    return result;
-  }
+	char *params = NULL;
+	int result =
+		duplicateString(name, "makeIndexLayout parameters", &params);
+	if (result != UDS_SUCCESS) {
+		return result;
+	}
 
-  // note file will be set to memory owned by params
-  result = parseLayoutString(params, parameterTable, COUNT_OF(parameterTable));
-  if (result != UDS_SUCCESS) {
-    FREE(params);
-    return result;
-  }
+	// note file will be set to memory owned by params
+	result = parseLayoutString(
+		params, parameterTable, COUNT_OF(parameterTable));
+	if (result != UDS_SUCCESS) {
+		FREE(params);
+		return result;
+	}
 
-  if (!file) {
-    FREE(params);
-    return logErrorWithStringError(UDS_INDEX_NAME_REQUIRED,
-                                   "no index specified");
-  }
+	if (!file) {
+		FREE(params);
+		return logErrorWithStringError(UDS_INDEX_NAME_REQUIRED,
+					       "no index specified");
+	}
 
-  IOFactory *factory = NULL;
-  result = makeIOFactory(file,
-                         newLayout ? FU_CREATE_READ_WRITE : FU_READ_WRITE,
-                         &factory);
-  FREE(params);
-  if (result != UDS_SUCCESS) {
-    return result;
-  }
-  IndexLayout *layout;
-  result = makeIndexLayoutFromFactory(factory, offset, size, newLayout, config,
-                                      &layout);
-  putIOFactory(factory);
-  if (result != UDS_SUCCESS) {
-    return result;
-  }
-  *layoutPtr = layout;
-  return UDS_SUCCESS;
+	IOFactory *factory = NULL;
+	result = makeIOFactory(file,
+			       newLayout ? FU_CREATE_READ_WRITE : FU_READ_WRITE,
+			       &factory);
+	FREE(params);
+	if (result != UDS_SUCCESS) {
+		return result;
+	}
+	IndexLayout *layout;
+	result = makeIndexLayoutFromFactory(
+		factory, offset, size, newLayout, config, &layout);
+	putIOFactory(factory);
+	if (result != UDS_SUCCESS) {
+		return result;
+	}
+	*layoutPtr = layout;
+	return UDS_SUCCESS;
 }
