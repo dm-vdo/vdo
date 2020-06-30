292d0ecb8a7c6e58f85b42d8883e59e50dbdbdf8
Red Hat VDO Team <vdo-devel@redhat.com>
2020-06-30 06:24:30 -0400

    145708 by sclafani on 2020/06/30 (pair: msakai) Merged changes 145550 and 145555 to Krusty 145550 by sweettea on 2020/06/24 (pair: jwiele) Recombine kernel and user permassert files [UDSUP-1]
    	.../permassert.c
    	.../permassertLinuxUser.c
    	Move handleAssertionFailure() and setExitOnAssertionFailure()
    	  to permassert.c, ifdef'd dependent on __KERNEL__ as appropriate.
    
    145710 by sclafani on 2020/06/30 (pair: msakai)
    Adapt linux-vdo to Krusty merge 145708.
    
    	.../Makefile
    	Removed files deleted by change 145550 (merged as 145708).
    
    Co-authored-by: sclafani@redhat.com
    Co-authored-by: msakai@redhat.com
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: jwiele@redhat.com

--- utils/uds/permassert.c
+++ utils/uds/permassert.c
@@ -16,13 +16,90 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/uds-releases/krusty/src/uds/permassert.c#1 $
+ * $Id: //eng/uds-releases/krusty/src/uds/permassert.c#2 $
  */
 
 #include "permassert.h"
 #include "permassertInternals.h"
 
 #include "errors.h"
+#include "logger.h"
+
+#ifndef __KERNEL__
+// Here begins a large block of userspace-only stuff.
+#ifdef NDEBUG
+#define DEBUGGING_OFF
+#undef NDEBUG
+#endif /* NDEBUG */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+#include "common.h"
+#include "stringUtils.h"
+#include "threads.h"
+
+#ifdef DEBUGGING_OFF
+static bool exitOnAssertionFailure = false;
+#else /* !DEBUGGING_OFF */
+static bool exitOnAssertionFailure = true;
+#endif /* DEBUGGING_OFF */
+
+static const char *EXIT_ON_ASSERTION_FAILURE_VARIABLE
+  = "UDS_EXIT_ON_ASSERTION_FAILURE";
+
+static once_state_t initOnce = ONCE_STATE_INITIALIZER;
+static struct mutex mutex    = { .mutex = MUTEX_INITIALIZER };
+
+/**********************************************************************/
+static void initialize(void)
+{
+  initializeMutex(&mutex, !DO_ASSERTIONS);
+  char *exitOnAssertionFailureString
+    = getenv(EXIT_ON_ASSERTION_FAILURE_VARIABLE);
+  if (exitOnAssertionFailureString != NULL) {
+    exitOnAssertionFailure
+      = (strcasecmp(exitOnAssertionFailureString, "true") == 0);
+  }
+}
+
+/**********************************************************************/
+bool setExitOnAssertionFailure(bool shouldExit)
+{
+  perform_once(&initOnce, initialize);
+  lockMutex(&mutex);
+  bool previousSetting = exitOnAssertionFailure;
+  exitOnAssertionFailure = shouldExit;
+  unlockMutex(&mutex);
+  return previousSetting;
+}
+
+// Here ends large block of userspace stuff.
+#endif // !__KERNEL__
+
+/**********************************************************************/
+__attribute__((format(printf, 4, 0)))
+void handleAssertionFailure(const char *expressionString,
+                            const char *fileName,
+                            int         lineNumber,
+                            const char *format,
+                            va_list     args)
+{
+  log_embedded_message(LOG_ERR, "assertion \"", format, args,
+                       "\" (%s) failed at %s:%d",
+                       expressionString, fileName, lineNumber);
+  log_backtrace(LOG_ERR);
+
+#ifndef __KERNEL__
+  perform_once(&initOnce, initialize);
+  lockMutex(&mutex);
+  if (exitOnAssertionFailure) {
+    __assert_fail(expressionString, fileName, lineNumber, __ASSERT_FUNCTION);
+  }
+  unlockMutex(&mutex);
+#endif // !__KERNEL__
+}
 
 /*****************************************************************************/
 int assertionFailed(const char *expressionString,
