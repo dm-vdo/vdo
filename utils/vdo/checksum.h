b379953ab337077922a94281fe8d022b739d66db
Red Hat VDO Team <vdo-devel@redhat.com>
2021-02-17 21:02:49 -0500

    148611 by sweettea on 2021/02/17 (pair: msakai) Pull our own checksumming out into its own header file.
    	Only two shipped files need our checksumming, so it seems wasteful
    	to spend a whole file on them, but there's no obvious place to put
    	it otherwise.
    
    	.../checksum.h
    	Add new header file that contains all checksumming constants and
    	  the checksumming function, for both kernel and userspace.
    
    	.../physicalLayer.h
    	.../types.h
    	Move update_crc32() declaration, typedef, and size constant to
    	  checksum.h.
    
    	.../physicalLayer.c
    	Deleted.
    
    	.../superBlockCodec.c
    	.../volumeGeometry.c
    	Added includes of checksum.h in users.
    
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: msakai@redhat.com

---
+++ utils/vdo/checksum.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA. 
+ *
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/checksum.h#1 $
+ */
+
+#ifndef CHECKSUM_H
+#define CHECKSUM_H
+
+#ifdef __KERNEL__
+#include <linux/crc32.h>
+#else
+#include <zlib.h>
+#endif // __KERNEL__
+
+/**
+ * A CRC-32 checksum
+ **/
+typedef uint32_t crc32_checksum_t;
+
+static const crc32_checksum_t INITIAL_CHECKSUM = 0xffffffff;
+
+enum {
+	/* The size of a CRC-32 checksum */
+	CHECKSUM_SIZE = sizeof(crc32_checksum_t),
+};
+
+/**
+ * A function to update a running CRC-32 checksum.
+ *
+ * @param crc     The current value of the crc
+ * @param buffer  The data to add to the checksum
+ * @param length  The length of the data
+ *
+ * @return The updated value of the checksum
+ **/
+static inline crc32_checksum_t update_crc32(crc32_checksum_t crc,
+					    const byte *buffer,
+			                    size_t length)
+{
+#ifdef __KERNEL__
+	/*
+	 * The kernel's CRC 32 implementation does not do pre- and post-
+	 * conditioning, so do it ourselves.
+	 */
+	return crc32(crc ^ 0xffffffff, buffer, length) ^ 0xffffffff;
+#else
+	return crc32(crc, buffer, length);
+#endif // __KERNEL__
+}
+
+#endif // CHECKSUM_H
--- vdo.spec
+++ vdo.spec
@@ -179,5 +179,5 @@ This package provides the user-space support tools for VDO.
 %{_mandir}/man8/vdoregenerategeometry.8.gz
 
 %changelog
-* Tue Feb 16 2021 - Red Hat VDO Team <vdo-devel@redhat.com> - 8.1.0.0-1
+* Wed Feb 17 2021 - Red Hat VDO Team <vdo-devel@redhat.com> - 8.1.0.0-1
 - See https://github.com/dm-vdo/vdo.git
