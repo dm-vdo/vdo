31b2be2590e268137c10d14005c3eb32a17239a0
Red Hat VDO Team <vdo-devel@redhat.com>
2021-07-22 20:52:15 -0400

    151263 by sclafani on 2021/07/21 (pair: msakai) Merged change 151084 to Krusty 151084 by sclafani on 2021/07/13 (pair: raeburn, msakai) Removed 152 bytes of wasted space from UDS request structures
    	The uds_request structure is embedded in a dedupe_context which is
    	embedded in every data_vio. It corresponds to struct
    	internal_request, which was 144 bytes, but can be reduced to 128
    	by using one-byte packed enums and swapping a field into an
    	alignment padding hole. But doing so would have had little effect
    	because it's the public struct uds_request that VDO uses, and that
    	has a fixed-size padding field that made it always be 280 bytes
    	regardless of the size of internal_request.
    
    	.../request.h
    	Declared index_region and request_action enums as __packed. Three
    	  fields use them in internal_request, so this saves nine bytes and
    	  avoids creating alignment holes.
    	Moved the zone_number field out from the middle of various
    	  one-byte fields and into an alignment hole after the uds_request
    	  fields. Combined with the enum change, this saves 16 bytes in
    	  total and allows internal_request to fit in 128 bytes (aka two
    	  cache lines, if aligned).
    
    	.../uds.h
    	Reduced the size of the private field padding at the end of
    	  struct uds_request from 25 * 8 to 6 * 8, making the structure be
    	  128 bytes in total. We no longer have any reason to pad the
    	  structure for binary API compatibility as we did when Albireo
    	  was implemented.
    
    151274 by msakai on 2021/07/21 (pair: sclafani)
    Merge 151102 to Krusty. [UDSUP-8]
    151102 by msakai on 2021/07/14 (pair: jwiele, sclafani)
    Expose uds_request structure. [UDSUP-8]
    
    	.../uds.h
    	.../request.h
    	Made struct uds_request fully visible by moving the content of
    	  struct internal_request into it, along with dependent types.
    	  Also added some documentation to fields.
    	Removed struct internal_request.
    
    	.../opaqueTypes.h
    	Changed Request typedef to use struct uds_request instead of
    	  struct internal_request. Removing the typedef is a much larger
    	  change.
    	Removed struct index_router declaration, which was unnecessary and
    	  is moot now that struct index_router is declared in uds.h.
    
    	.../request.c
    	Respelled the zeroing of internal fields.
    	Removed unneccessary type casting.
    
    151279 by msakai on 2021/07/21 (pair: sclafani)
    Merge 151119 and 151121 to Krusty. [ALB-3073] [BZ 1966824]
    151119 by msakai on 2021/07/15 (pair: jwiele)
    Respell rebuild algorithm to remove allocation. [ALB-3073] [BZ 1966824]
    
    	.../volume.c
    	Respelled boundary-finding algorithm to not need an allocated
    	  array. Instead, skip over any out-of-order chapter while searching
    	  and add it back in at the end if necessary.
    	Rearranged highest virtual chapter number logic into a while loop.
    
    151121 by msakai on 2021/07/15 (pair: jwiele)
    Fix typo in comment. [ALB-3073] [BZ 1966824]
    
    	.../volume.c
    	Fix typo in comment added in change 151119.
    
    Co-authored-by: sclafani@redhat.com
    Co-authored-by: msakai@redhat.com
    Co-authored-by: raeburn@redhat.com
    Co-authored-by: jwiele@redhat.com

--- utils/vdo/fileLayer.c
+++ utils/vdo/fileLayer.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/user/fileLayer.c#21 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/user/fileLayer.c#22 $
  */
 
 #include "fileLayer.h"
@@ -292,7 +292,7 @@ static int setupFileLayer(const char     *name,
   }
 
   size_t     nameLen = strlen(name) + 1;
-  FileLayer *layer;
+  FileLayer *layer   = NULL;
   result
     = UDS_ALLOCATE_EXTENDED(FileLayer, nameLen, char, "file layer", &layer);
   if (result != UDS_SUCCESS) {
