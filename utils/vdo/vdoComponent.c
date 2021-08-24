3df59425e416b5895ebb16f615ff1daf2412069c
Red Hat VDO Team <vdo-devel@redhat.com>
2021-08-24 15:46:27 -0400

    152080 by sweettea on 2021/08/24 (pair: corwin) Tighten kernel-mode assertion (and remove a reference to FileLayer)
    	.../vdoComponent.c
    	Changed to assert precise equality in kernel mode.
    	Moved comment referring to userspace testing to a non-kernel ifdef.
    
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: corwin@redhat.com

--- utils/vdo/vdoComponent.c
+++ utils/vdo/vdoComponent.c
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/linux-vdo/src/c++/vdo/base/vdoComponent.c#15 $
+ * $Id: //eng/linux-vdo/src/c++/vdo/base/vdoComponent.c#16 $
  */
 
 #include "vdoComponent.h"
