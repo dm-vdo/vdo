721b9e10290276f5ad18ab75293a37b3fb0b74d2
Red Hat VDO Team <vdo-devel@redhat.com>
2021-09-14 20:19:58 -0400

    152369 by sweettea on 2021/09/13 (pair: jwiele) Merge 152340 and 152344 to Lisa. 152340 by jwiele on 2021/09/10 (pair: msakai, sclafani) Fix the issue with conflicting stdarg.h includes.
    	Different kernel versions get stdarg.h either from a header
    	file provided by the compiler <stdarg.h> or from a header file
    	in the kernel source tree <linux/stdarg.h>. It should suffice
    	to include <linux/kernel.h> as long as <linux/kernel.h>
    	includes stdarg.h correctly.
    
    	.../memoryAlloc.h
    	Include kernel.h to get stdarg.h in kernel space and include
    	  stdarg.h explicitly in user space.
    
    	.../stringUtils.h
    	Only include stdarg.h in user space since it will be included
    	  by kernel.h in kernel space.
    
    152344 by sclafani on 2021/09/10 (pair: jwiele)
    Removed stdargs.h include from memoryAlloc.h
    
    	.../memoryAlloc.h
    	Removed stdargs.h include and the kernel.h include that
    	  was added to pick it up since this file doesn't declare
    	  any variadic functions.
    
    Co-authored-by: sweettea@redhat.com
    Co-authored-by: jwiele@redhat.com
    Co-authored-by: msakai@redhat.com
    Co-authored-by: sclafani@redhat.com

--- utils/uds/stringUtils.h
+++ utils/uds/stringUtils.h
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA. 
  *
- * $Id: //eng/uds-releases/lisa/src/uds/stringUtils.h#2 $
+ * $Id: //eng/uds-releases/lisa/src/uds/stringUtils.h#3 $
  */
 
 #ifndef STRING_UTILS_H
